aa7a504984c64d45efc6371d59c20cebcd31814f
felissedano <contact@felissedano.com>
2026-09-22 22:55:46 -0400

    feat: update c and c++ shell.nix template

--- Templates/cpp-shell.nix
+++ Templates/cpp-shell.nix
@@ -1,11 +1,25 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+pkgs.mkShell.override
+  {
+    # Override stdenv in order to change compiler:
+    # stdenv = pkgs.clangStdenv;
+  }
+  {
+    packages =
+      with pkgs;
+      [
+        clang-tools
+        cmake
+        codespell
+        cppcheck
+        doxygen
+        gtest
+        lcov
+        vcpkg
+        vcpkg-tool
+      ]
+      ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ gdb ];
+  }
 
-pkgs.mkShell {
-  packages = with pkgs; [
-    llvmPackages.clang
-    llvmPackages.libcxx
-    llvmPackages.clang-tools
-  ];
-}
